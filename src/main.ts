3035a3b53029d8d3eefc98c9ee032719e44cd48b
Khushal Soni <khushalsoni089@gmail.com>
2024-08-25 22:28:56 +0530

    fix: updated cors config

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // * Enable global validation
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(3000);
 }
 bootstrap();
